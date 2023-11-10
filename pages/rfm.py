84d99d869ba41336145649c73a5259b2e71a3ee4
Mardianto-Mardianto <57285914+Mardianto-Mardianto@users.noreply.github.com>
2023-11-11 00:26:00 +0700

    Update rfm.py

--- pages/rfm.py
+++ pages/rfm.py
@@ -6,11 +6,10 @@ import seaborn as sns
 import datetime
 from babel.numbers import format_currency
 
-customer_df = pd.read_csv("E-Commerce Public Dataset/customers_dataset.csv")
-seller_df = pd.read_csv("E-Commerce Public Dataset/sellers_dataset.csv")
-rfm_df = pd.read_csv("E-Commerce Public Dataset/rfm_df.csv")
-rfm_rank_df = pd.read_csv("E-Commerce Public Dataset/rfm_rank.csv")
-order_items_product_df = pd.read_csv("E-Commerce Public Dataset/order_items_product_df.csv")
+customer_df = pd.read_csv("../E-Commerce Public Dataset/customers_dataset.csv")
+seller_df = pd.read_csv("../E-Commerce Public Dataset/sellers_dataset.csv")
+rfm_df = pd.read_csv("../E-Commerce Public Dataset/rfm_df.csv")
+order_items_product_df = pd.read_csv("../E-Commerce Public Dataset/order_items_product_df.csv")
 
 with st.sidebar:
     # Menambahkan logo perusahaan
